97332e8af0436ce271e05560f7085bc4acb73760
YanNaingHt <94881139+yannainghtet@users.noreply.github.com>
2023-10-31 02:26:31 +0630

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -10,7 +10,7 @@ window.onload = function() {
 
 
 
-var socket = io.connect('http://127.0.0.1:5000')
+var socket = io.connect('https://blackjack-7w0d.onrender.com:5000')
 
 
 // upon player connection, start the game
