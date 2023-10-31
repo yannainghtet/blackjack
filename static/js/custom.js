253f6b9ec1db082e3dba912e5981fce527a3aed9
YanNaingHt <94881139+yannainghtet@users.noreply.github.com>
2023-10-31 09:19:37 +0630

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -10,7 +10,7 @@ window.onload = function() {
 
 
 
-var socket = io.connect('http://127.0.0.1:5000')
+var socket = io()
 
 // upon player connection, start the game
 socket.on('connect', function(){
