6e020021beb8ba73dcb9b7d14f9dbabbe5772a81
YanNaingHt <94881139+yannainghtet@users.noreply.github.com>
2023-10-31 09:59:56 +0630

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,7 +4,7 @@
 
 
 
-var socket = io.connect("https://blackjackeds.onrender.com/")
+var socket = io.connect("https://blackjackedscardgame.onrender.com/")
 console.log("Connect to the server")
 // upon player connection, start the game
 socket.on('connect', function(){
