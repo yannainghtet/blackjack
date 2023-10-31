04863a31070856c0013cea22107a9d26b38e2fdc
YanNaingHt <94881139+yannainghtet@users.noreply.github.com>
2023-10-31 10:24:48 +0630

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,7 +4,7 @@
 
 
 
-var socket = io.connect("https://blackjackynh.onrender.com/")
+var socket = io.connect("https://blackjackgame.onrender.com/")
 
 // upon player connection, start the game
 socket.on('connect', function(){
